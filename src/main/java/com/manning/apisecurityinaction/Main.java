e5c593b7796983b3adb937d2833876cfc3cdccf9
yuhueilee <wendylee70127@gmail.com>
2023-01-03 10:46:43 +0800

    Create project using maven

---
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -0,0 +1,35 @@
+package com.manning.apisecurityinaction;
+
+import com.manning.apisecurityinaction.controller.SpaceController;
+
+import java.nio.file.*;
+
+import org.dalesbred.*;
+import org.h2.jdbcx.*;
+import org.json.JSONObject;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String... args) throws Exception {
+        var datasource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter", "password");
+        var database = Database.forDataSource(datasource);
+        createTables(database);
+
+        var spaceController = new SpaceController(database);
+        post("/spaces", spaceController::createSpace);
+
+        after((request, response) -> {
+            response.type("application/json");
+        });
+
+        internalServerError(new JSONObject().put("error", "internal server error").toString());
+        notFound(new JSONObject().put("error", "not found").toString());
+    }
+
+    private static void createTables(Database database) throws Exception {
+        var path = Paths.get(Main.class.getResource("/schema.sql").toURI());
+        database.update(Files.readString(path));
+    }
+}
---
+++ src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
@@ -0,0 +1,37 @@
+package com.manning.apisecurityinaction.controller;
+
+import java.sql.SQLException;
+
+import org.dalesbred.Database;
+import org.json.*;
+import spark.*;
+
+public class SpaceController {
+
+    private final Database database;
+
+    public SpaceController(Database database) {
+        this.database = database;
+    }
+
+    public JSONObject createSpace(Request request, Response response) throws SQLException {
+        var json = new JSONObject(request.body());
+        var spaceName = json.getString("name");
+        var owner = json.getString("owner");
+        return database.withTransaction(tx -> {
+            var spaceId = database.findUniqueLong(
+                    "SELECT NEXT VALUE FOR space_id_seq;");
+            // WARNING: this next line of code contains a
+            // security vulnerability!
+            database.updateUnique(
+                    "INSERT INTO spaces(space_id, name, owner) " +
+                            "VALUES(" + spaceId + ", '" + spaceName +
+                            "', '" + owner + "');");
+            response.status(201);
+            response.header("Location", "/spaces/" + spaceId);
+            return new JSONObject()
+                    .put("name", spaceName)
+                    .put("uri", "/spaces/" + spaceId);
+        });
+    }
+}
